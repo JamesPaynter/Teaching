8a7cca9088501e92c16f61bf51c2149d6278b237
James Paynter <45152710+JamesPaynter@users.noreply.github.com>
2021-04-19 16:42:53 +1000

    Create week6.py

---
+++ COMP10001/week6.py
@@ -0,0 +1,77 @@
+# Debug the following code
+
+def func1(xvar):
+    " sum over x. "
+    yvar == 0
+    for i in xvar:
+        yvar += xvar[i]
+    print(yvar)
+
+def func2():
+    x = ([0, 1, 2, 3])
+    y = func1(xvar)
+    if yvar:
+        print("The sum of x is {yvar}")
+
+def func34(num1 = None, num2 = None):
+    # sums two numbers
+    sum = num1 + num2
+    return sum if true else False
+
+def func4():
+    """ find the sum """
+    pop = 0
+    # y = [[1]m [x], [x-3], [x**2]]
+    for x in y:
+        print(x)
+        pop += y[x]
+
+    return(pop)
+
+def fun5():
+    func4(x = 5)
+    func3(num1 = 4)
+
+def func5():
+    ls = []
+    i = 0
+    while i:
+        ls.append(i)
+        func1(ls)
+        i+=1
+        if i > 10:
+            Break
+
+
+x = 10
+y = 12
+def modulo(a, b):
+    return x % y
+
+def modulo2(x, y):
+    return x % y
+
+def another_function(a, b):
+    a += b
+    c = b
+    b = a - c
+    return b, a
+
+def areaofacircle(d):
+    return 3.14 * d **2
+
+length = 2
+print("a square of side length {length}")
+print("can fit a circle of area {areaofacircle[length]}")
+print("Crazy!!")
+
+def volumesphere(radius):
+    "returns the volume of a sphere"
+    return 4 / 3 pi r ** 3
+
+def spheresurfacearea(r):
+    return "the surface area of a sphere"
+    print(4 * 3.14 * r ** 2)
+
+if __name__ == '__main__':
+    func5()
